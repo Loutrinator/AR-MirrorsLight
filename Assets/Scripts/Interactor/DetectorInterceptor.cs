e15714add6fa47234f9aa7f074c0724ce0b7c241
Cyphall <cyphallbraus@gmail.com>
2021-02-09 22:40:56 +0100

    Added detector

---
+++ Assets/Scripts/Interactor/DetectorInterceptor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interactor
+{
+    public class DetectorInterceptor : MonoBehaviour, ILaserInteractor
+    {
+        public UnityEvent DetectorActivation;
+        public UnityEvent DetectorDesactivation;
+
+        private bool _lastFrameHit;
+        private bool _thisFrameHit;
+
+        public void OnHit(RaycastHit hit, Laser laser)
+        {
+            laser.CastChild(hit.point + laser.transform.forward * 0.001f, laser.transform.forward);
+
+            _thisFrameHit = true;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_lastFrameHit && _thisFrameHit)
+            {
+                OnDetectorActivation();
+            }
+            else if (_lastFrameHit && !_thisFrameHit)
+            {
+                OnDetectorDesactivation();
+            }
+
+            _lastFrameHit = _thisFrameHit;
+            _thisFrameHit = false;
+        }
+
+        private void OnDetectorActivation()
+        {
+            DetectorActivation.Invoke();
+        }
+
+        private void OnDetectorDesactivation()
+        {
+            DetectorDesactivation.Invoke();
+        }
+    }
+}
