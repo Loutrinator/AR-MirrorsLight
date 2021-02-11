8021c2eb9e634077bdd3c80f872aaf69bb4e6c46
Cyphall <cyphallbraus@gmail.com>
2021-02-11 11:38:27 +0100

    Fixed dumb architecture

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,14 +1,7 @@
 ﻿public class LevelManager : LevelManagerBase
 {
-    private GameManager _gameManager;
-    
     public void Win()
     {
         _gameManager.Win();
-    }
-
-    public override void SetGameManager(GameManager gameManager)
-    {
-	    _gameManager = gameManager;
     }
 }
--- Assets/Scripts/LevelManagerBase.cs
+++ Assets/Scripts/LevelManagerBase.cs
@@ -2,5 +2,10 @@ using UnityEngine;
 
 public abstract class LevelManagerBase : MonoBehaviour
 {
-	public abstract void SetGameManager(GameManager gameManager);
+	protected GameManager _gameManager;
+	
+	public void SetGameManager(GameManager gameManager)
+	{
+		_gameManager = gameManager;
+	}
 }
