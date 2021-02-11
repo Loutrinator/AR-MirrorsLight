c32c9204f5b668e3e1025d291b3ea42360a4ca43
Cyphall <cyphallbraus@gmail.com>
2021-02-11 12:02:00 +0100

    Added splitter

---
+++ Assets/Scripts/Interactor/SplitterInteractor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Interactor
+{
+    public class SplitterInteractor : MonoBehaviour, ILaserInteractor
+    {
+        public void OnHit(RaycastHit hit, Laser laser)
+        {
+            Vector3 leftDir = Quaternion.AngleAxis(-60, transform.up) * -transform.right;
+            Vector3 rightDir = Quaternion.AngleAxis(60, transform.up) * -transform.right;
+            
+            laser.CastChild(transform.position + transform.up * 0.05522725f + leftDir * 0.02f, leftDir);
+            laser.CastChild(transform.position + transform.up * 0.05522725f + rightDir * 0.02f, rightDir);
+        }
+    }
+}
